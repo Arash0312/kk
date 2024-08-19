06e919189eeb11ebed6697f8f1a761b009ae744e
Arash0312 <arashm7100@gmail.com>
2024-08-19 19:02:31 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,43 +4,43 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Riding Extreme 3D',
             appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
             promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-            attempts: 25,
-            timing: 30000, // 30 seconds
+            timing: 20000, // 20 seconds
+            attempts: 19,
         },
         2: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-            timing: 30000, // 30 seconds
-            attempts: 20,
+            timing: 20000, // 20 seconds
+            attempts: 10,
         },
         3: {
             name: 'My Clone Army',
             appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
             promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-            timing: 30000, // 30 seconds
-            attempts: 30,
+            timing: 120000, // 120 seconds
+            attempts: 11,
         },
         4: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-            timing: 30000, // 30 seconds
-            attempts: 15,
+            timing: 20000, // 20 seconds
+            attempts: 10,
         },
         5: {
             name: 'Merge Away',
             appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
             promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
-            timing: 30000, // 30 seconds
-            attempts: 25,
+            timing: 21000, // 21 seconds
+            attempts: 15,
         },
         6: {
             name: 'Twerk Race 3D',
             appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            timing: 30000, // 30 seconds
-            attempts: 20,
+            timing: 23000, // 23 seconds
+            attempts: 15,
         }
     };
 
@@ -207,7 +207,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         if (!response.ok) {
-            throw new Error('پروکسی تلگرامو تغییر بده');
+            throw new Error('Failed to login');
         }
 
         const data = await response.json();
